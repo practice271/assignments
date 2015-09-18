68ef2adb2b577107bde4bc22e47512614f564993
GuzelGarifullina <GiseleGarifullina@gmail.com>
2015-09-18 20:01:18 +0300

    Tests for maxway of tree

--- src/test/kotlin/hw01/TreeTest.kt
+++ src/test/kotlin/hw01/TreeTest.kt
@@ -1,7 +1,26 @@
+/* Tests for heap sort made by Guzel Garifullina
+   Estimated time 15 minutes
+   real time      30 minutes
+*/
+
 package hw01
 
-/**
- * Created by guzel on 18.09.15.
- */
+import org.junit.Test
+import kotlin.test.assertEquals
+
 public class TreeTest {
+    @Test fun Test1() {
+      val tree = generateTree(0, 4)
+      assertEquals(9, tree.maxWay(0))
+    }
+
+    @Test fun Test2() {
+      val tree = generateTree(-7, 0)
+      assertEquals(-7, tree.maxWay(-100))
+    }
+
+    @Test fun Test3() {
+      val tree = generateTree(0, 16)
+      assertEquals(65, tree.maxWay(0))
+    }
 }
