863f273667da579e757306a2bab0ada262f44ff5
Mikhail Kita <mihail.kita@gmail.com>
2015-09-19 12:02:35 +0300

    Updated test for 2nd homework

--- src/test/kotlin/homework/hw02/hw02Test.kt
+++ src/test/kotlin/homework/hw02/hw02Test.kt
@@ -73,17 +73,17 @@ public class hw02Test {
         val OSList = arrayOf("Linux", "Windows", "FreeBSD", "OS X", "Linux", "Windows",
                 "Linux", "Windows", "OS X", "FreeBSD")
 
-        var labels = arrayOf(false, true, false, false, false, false, false, true, false, false)
+        var labels = arrayOf(true, false, false, false, false, false, false, false, false, false)
 
         val aList =
-                arrayOf(listOf(1, 4), listOf(0, 2), listOf(1, 3, 4), listOf(2), listOf(0, 2, 5), listOf(4),
-                        listOf(7, 8), listOf(6, 9), listOf(6, 9), listOf(7, 8))
+                arrayOf(listOf(1), listOf(2), listOf(3), listOf(4), listOf(5), listOf(6),
+                        listOf(7), listOf(8), listOf(9), listOf())
 
         val graph = ComputerGraph (OSList, labels, aList)
         val network = LocalNetwork (graph)
 
-        for (i in 0..100000) {
-            network.start(1.0) //probability of infection = 0
+        for (i in 0 .. 100000) {
+            network.start(1.0) //probability of infection = 1
         }
         assertEquals(1, network.infectedNumber())
     }
