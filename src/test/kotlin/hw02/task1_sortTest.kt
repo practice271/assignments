8f03aa0bd4e8e962dc02e29e343348e1b4501999
onewhl <prystjam@gmail.com>
2015-09-25 21:03:41 +0300

    added first task
    added first task and tests

---
+++ src/test/kotlin/hw02/task1_sortTest.kt
@@ -0,0 +1,51 @@
+
+/**
+ * Created by Zarina Kurbatova on 19.09.2015.
+ */
+
+package hw02
+import org.junit.Test
+import kotlin.test.assertEquals
+
+public class task1_Test {
+    fun equal(arr1: Array<Int>, arr2: Array<Int>): Boolean {
+
+        if (arr1.size() != arr2.size()) return false
+
+        var i: Int = arr1.size()-1
+        while(i>=0) {
+            if (arr1[i] != arr2[i]) {return false}
+            i--
+        }
+
+        return true
+    }
+
+    @Test fun test1() {
+        var arr1 = arrayOf(7,2,3,9,1,6,11,17,5)
+        val arr2 = arrayOf(1,2,3,5,6,7,9,11,17)
+        heapSort(arr1)
+        assertEquals(true, equal(arr1,arr2))
+    }
+
+    @Test fun test2() {
+        var arr1 = arrayOf(19,3,24,4,5,9,0,1)
+        val arr2 = arrayOf(0,1,3,4,5,9,19,24)
+        heapSort(arr1)
+        assertEquals(true, equal(arr1,arr2))
+    }
+
+    @Test fun test3() {
+        var arr1 = arrayOf(6,16,1,25,7,4,8,2,0)
+        val arr2 = arrayOf(0,1,2,4,6,7,8,16,25)
+        heapSort(arr1)
+        assertEquals(true, equal(arr1,arr2))
+    }
+
+    @Test fun test4() {
+        var arr1 = arrayOf(10,9,8,7,6,5,4,3,2,1,0)
+        val arr2 = arrayOf(0,1,2,3,4,5,6,7,8,9,10)
+        heapSort(arr1)
+        assertEquals(true, equal(arr1,arr2))
+    }
+}
---
+++ src/test/kotlin/hw02/task4_Test.kt
@@ -0,0 +1,28 @@
+/**
+ * Created by z on 19.09.2015.
+ */
+
+package hw02
+import org.junit.Test
+import kotlin.test.assertEquals
+
+public class task4_Test {
+    val x = S(S(S(Zero())))
+    val y = S(S(S(S(Zero()))))
+
+    @Test fun sumTest() {
+        assertEquals(7,toInt(sum(x,y)))
+    }
+
+    @Test fun multTest() {
+        assertEquals(12, toInt(mult(x,y)))
+    }
+
+    @Test fun subTest() {
+        assertEquals(1, toInt(sub(y,x)))
+    }
+
+    @Test fun degTest() {
+        assertEquals(81, toInt(deg(x,y)))
+    }
+}
