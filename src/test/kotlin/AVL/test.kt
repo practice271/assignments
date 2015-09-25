6e77fc1c39b679e728c05b19e44bc4ea502451a8
Anna Maryina <maryina.anna.va@gmail.com>
2015-09-25 23:47:19 +0300

    Try some test

---
+++ src/test/kotlin/AVL/test.kt
@@ -0,0 +1,12 @@
+package AVL
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+public class Tester {
+    Test fun helloTest() {
+        assertEquals("Hello", hello())
+    }
+
+    fun hello() = "Hello"
+}
