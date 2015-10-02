47792b661e39984a2b75fc9904392f9b97a05c9c
Roru <vovayumatov@gmail.com>
2015-10-02 03:07:41 +0300

    Added homework 3

--- src/main/kotlin/hw03/avltree.kt
+++ src/main/kotlin/hw03/avltree.kt
@@ -6,28 +6,28 @@ internal class Node(internal var key: Int, internal var left: Node?, internal va
 
     private fun height(): Int = 1 + Math.max((right?.height() ?: 0), (left?.height() ?: 0))
 
-    internal fun balance(): Int = right?.height() ?: 0 - (left?.height() ?: 0)
+    private fun balance(): Int = (right?.height() ?: 0) - (left?.height() ?: 0)
 
-    internal fun ltrotate() {
+    private fun ltrotate() {
         if(right != null) {
-        left = Node(key, left, right!!.left)
-        key = right!!.key
-        right = right!!.right
+        left = Node(key, left, right?.left)
+        key = right?.key ?: 0
+        right = right?.right
         }
     }
-    internal fun rgrotate() {
+    private fun rgrotate() {
         if(left != null) {
-            right = Node(key, left!!.right, right)
-            key = left!!.key
-            left = left!!.left
+            right = Node(key, left?.right, right)
+            key = left?.key ?: 0
+            left = left?.left
         }
     }
 
-    internal fun rotateRL() {
+    private fun rotateRL() {
         right?.rgrotate()
         ltrotate()
     }
-    internal fun rotateLR() {
+    private fun rotateLR() {
         left?.ltrotate()
         rgrotate()
     }
@@ -50,7 +50,6 @@ internal class Node(internal var key: Int, internal var left: Node?, internal va
         }
     }
 
-
 }
 
 
@@ -61,17 +60,20 @@ internal open class AVLtree() {
 
     private fun ins(k: Int, nd: Node?): Node {
         nd ?: return Node(k, null, null)
-        if(k == nd.key)
-            return nd
-        else if(k > nd.key) {
-            nd.right = ins(k, nd.right)
-            nd.rebalance()
-            return nd
-        }
-        else {
-            nd.left = ins(k, nd.left)
-            nd.rebalance()
-            return nd
+        when {
+            k == nd.key -> return nd
+
+            k > nd.key -> {
+                nd.right = ins(k, nd.right)
+                nd.rebalance()
+                return nd
+            }
+
+            else -> {
+                nd.left = ins(k, nd.left)
+                nd.rebalance()
+                return nd
+            }
         }
     }
     internal fun insert(k: Int) {
@@ -89,19 +91,20 @@ internal open class AVLtree() {
         }
 
         nd ?: return null
-        if(k < nd.key)
-            nd.left = del(k, nd.left)
-        else if(k > nd.key)
-            nd.right = del(k, nd.right)
-        else {
-            nd.right ?: return nd.left
-            val min = nd.right!!.minnode()
-            min.right = nd.right!!.delmin()
-            min.left = nd.left
-            min.rebalance()
-            return min
+        when {
+            k < nd.key -> nd.left = del(k, nd.left)
+
+            k > nd.key -> nd.right = del(k, nd.right)
+            else -> {
+                nd.right ?: return nd.left
+                val min = nd.right!!.minnode()
+                min.right = nd.right!!.delmin()
+                min.left = nd.left
+                min.rebalance()
+                return min
+            }
         }
-        nd.balance()
+        nd.rebalance()
         return nd
     }
     internal fun delete(k: Int) {
@@ -111,17 +114,16 @@ internal open class AVLtree() {
 
     private fun search(k: Int, nd: Node?): Int {
         nd ?: return -1
-        return if(k == nd.key)
-            nd.key
-        else if(k > nd.key)
-            search(k, nd.right)
-        else
-            search(k, nd.left)
+        return when {
+            k == nd.key -> nd.key
+            k > nd.key -> search(k, nd.right)
+            else -> search(k, nd.left)
+        }
     }
     internal fun search(k: Int): Int = search(k, root)
 
 
-    internal fun toText() : String {
+   /* internal fun toText() : String {
         fun Node.toText_(): List<String> {
             val lText = left?.toText_()?.map { "|  $it" } ?: listOf("|  _\n")
             val rText = right?.toText_()?.map { "|  $it" } ?: listOf("|  _\n")
@@ -132,26 +134,5 @@ internal open class AVLtree() {
         val lines = root?.toText_() ?: listOf("_\n")
         lines.forEach { builder.append(it) }
         return builder.toString()
-    }
-}
-
-
-fun main(args: Array<String>) {
-    fun Node.print(): String {
-        return key.toString() + "  " + (right?.print()?:"") + "  " + (left?.print()?:"")
-    }
-
-    val t = AVLtree()
-    t.insert(2)
-    t.insert(10)
-    t.insert(14)
-    println(t.toText())
-    t.insert(6)
-    t.insert(12)
-    t.insert(100)
-    println(t.toText())
-    t.insert(15)
-    t.insert(1)
-    t.root?.rebalance()
-    println(t.toText())
+    } */
 }
--- src/test/kotlin/hw03/AVLTest.kt
+++ src/test/kotlin/hw03/AVLTest.kt
@@ -4,23 +4,52 @@ import org.junit.Test
 import kotlin.test.assertEquals
 
 public class AVLTest {
-    private fun Node.print(): String {
-        //this ?: return "Tree is empty"
-        return key.toString() + "  " + (right?.print()?:"") + "  " + (left?.print()?:"")
+    private fun AVLtree.clrstr() : String {
+        fun Node.print() : String {
+            val lText = left?.print() ?: ""
+            val rText = right?.print() ?: ""
+            val vText = key.toString() + " "
+            return vText + lText + rText
+        }
+        
+        return root?.print() ?: "TIE"
     }
-    private fun printEmpty(n: Node): String = "TIE"
 
-    private val t = AVLtree()
-    init {
-        t.insert(2)
-        t.insert(10)
-        t.insert(14)
-        t.insert(6)
-        t.insert(12)
-        t.insert(100)
-        t.insert(15)
+
+    @Test fun insertion() {
+        val t: AVLtree = AVLtree()
+        assertEquals("TIE", t.clrstr())
+
+        t.insert(4)
+        assertEquals("4 ", t.clrstr())
+
+        t.insert(2); t.insert(3)
+        assertEquals("3 2 4 ", t.clrstr())
+
         t.insert(1)
+        assertEquals("3 2 1 4 ", t.clrstr())
     }
 
-    //@Test fun delete
+    @Test fun deletion() {
+        val t: AVLtree = AVLtree()
+        t.delete(3)
+        assertEquals("TIE", t.clrstr())
+
+        t.insert(2); t.insert(10); t.insert(14); t.insert(6); t.insert(12); t.insert(100); t.insert(15); t.insert(17)
+        t.delete(10)
+        assertEquals("12 2 6 15 14 17 100 ", t.clrstr())
+
+        t.delete(14)
+        assertEquals("12 2 6 17 15 100 ", t.clrstr())
+    }
+
+    @Test fun searching() {
+        val t: AVLtree = AVLtree()
+        assertEquals(-1, t.search(5))
+
+        t.insert(2); t.insert(10); t.insert(14); t.insert(6); t.insert(12); t.insert(100); t.insert(15); t.insert(17)
+        assertEquals(10, t.search(10))
+        assertEquals(6, t.search(6))
+        assertEquals(2, t.search(2))
+    }
 }
