e003346f1a4de14e29aa77f9674440aad983508e
Aleksei Alekseev <alexey_alexeev@bk.ru>
2015-10-14 03:24:51 +0300

    Fixed hw05

--- src/main/kotlin/homework/hw05/increment.kt
+++ src/main/kotlin/homework/hw05/increment.kt
@@ -3,44 +3,34 @@
 
 package homework.hw05
 
-public fun newThread(block : () -> Unit) : Thread {
-    val thread = object : Thread() {
-        public override fun run() {
-            block()
-        }
-    }
-    thread.start()
-    thread.join()
-    return thread
-}
-
 public fun increment(x : Int, applyNum : Int, threadNum : Int) : Int {
     var count = x
     val threads = Math.min(applyNum, threadNum)
-    val step = applyNum / threads
-    for (i in 1..threads) {
-        newThread(fun() {
-            for (j in 1..step)
-                count++
-        })
+    val step : Int = applyNum / threads
+    val threadArray = Array<Thread>(threads, { _ ->  Thread(fun() {for (j in 1..step) count++})})
+    for (t in threadArray) {
+        t.start()
+    }
+    for (t in threadArray) {
+        t.join()
     }
     val temp = applyNum - step * threads
     if (temp != 0) for (k in 1..temp) {count++}
-//  println("result = " + count)
     return count
 }
 
-/*
+
 public fun measureTimeMillis(block : () -> Unit) : Long {
     val start = System.currentTimeMillis()
     block()
     return System.currentTimeMillis() - start
 }
 
+/*
 fun main (args : Array<String>){
-    println("Elapsed Time: " + measureTimeMillis(fun(){println(increment(0,10000,1))}))
-    println("Elapsed Time: " + measureTimeMillis(fun(){println(increment(0,10000,2))}))
-    println("Elapsed Time: " + measureTimeMillis(fun(){println(increment(0,10000,3))}))
-    println("Elapsed Time: " + measureTimeMillis(fun(){println(increment(0,10000,1000))}))
+    println("Elapsed Time: " + measureTimeMillis(fun(){println(increment(0,100000,1))}))
+    println("Elapsed Time: " + measureTimeMillis(fun(){println(increment(0,100000,2))}))
+    println("Elapsed Time: " + measureTimeMillis(fun(){println(increment(0,100000,3))}))
+    println("Elapsed Time: " + measureTimeMillis(fun(){println(increment(0,100000,4))}))
 }
 */
