5444b5134bab20d7da381a760499826ded15836d
Kirill Smirenko <cyrussmith1865@gmail.com>
2015-10-23 00:34:49 +0300

    almost finished core

---
+++ src/main/kotlin/homework/hw06/cli/main.kt
@@ -0,0 +1,15 @@
+package homework.hw06.cli
+
+/**
+ * TicTacToe command-line interface .
+ * expected time: 4 h
+ * actual time: (19:30-20:00, 23:00-)
+ * @author Kirill Smirenko, group 271
+ */
+
+fun loop() {
+
+}
+
+fun main(args: Array<String>) {
+}
--- src/main/kotlin/homework/hw06/core.kt
+++ src/main/kotlin/homework/hw06/core.kt
@@ -3,10 +3,10 @@ package homework.hw06
 /**
  * TicTacToe logic.
  * expected time: 4 h
- * actual time: (19:30-)
+ * actual time: (19:30-20:00, 23:00-)
  * @author Kirill Smirenko, group 271
  */
-class Core {
+class Core private constructor () {
     private val gridSize = 3
     private val cellNone = 0
     private val cellX = 1
@@ -15,26 +15,60 @@ class Core {
     private var gridContent = Array(gridSize, { IntArray(gridSize) })
     private var crossesMove = true
 
+    companion object {
+        /**
+         * A Core singleton.
+         */
+        public val instance = Core()
+    }
+
     public fun getGridSize() = gridSize
 
+    /**
+     * Saves info about last move, and returns whether the game has finished, or the input info was erroneous.
+     */
     public fun makeMove(x : Int, y : Int) : ErrorCode {
         if (gridContent[x][y] == cellNone) {
             gridContent[x][y] = if (crossesMove) cellX else cellO
-            return if (isGameFinished()) ErrorCode.WIN else ErrorCode.OK
+            return if (isGameFinished(x, y)) ErrorCode.WIN else ErrorCode.OK
         }
         else {
             return ErrorCode.ERROR
         }
     }
 
+    /**
+     * Restarts the game.
+     */
     public fun restart() {
         gridContent = Array(gridSize, { IntArray(gridSize) })
         crossesMove = true
     }
 
-    private fun isGameFinished() : Boolean {
-        return false
-        // TODO
+    /**
+     * Checks whether the game was finished after a symbol was put into cell [[x], [y]].
+     */
+    private fun isGameFinished(x : Int, y : Int) : Boolean {
+        var cellValue = gridContent[x][y]
+        var bingoRow = true
+        for (j in 0..gridSize - 1)
+            bingoRow = bingoRow && (cellValue == gridContent[x][j])
+        var bingoColumn = true;
+        for (i in 0..gridSize - 1)
+            bingoColumn = bingoColumn && (cellValue == gridContent[i][y])
+        var bingoDiag1 = false
+        if (x == y) {
+            bingoDiag1 = true;
+            for (i in 0..gridSize - 1)
+                bingoDiag1 = bingoDiag1 && (cellValue == gridContent[i][i])
+        }
+        var bingoDiag2 = false
+        if (x == gridSize - y - 1) {
+            bingoDiag2 = true;
+            for (i in 0..gridSize - 1)
+                bingoDiag2 = bingoDiag2 && (cellValue == gridContent[i][gridSize - i - 1])
+        }
+        return bingoRow || bingoColumn || bingoDiag1 || bingoDiag2
     }
 
     public enum class ErrorCode {
