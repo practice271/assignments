29a3d09507844d5ffe0758de55076187e20eb12b
elizakuz <kuzminaeliz@gmail.com>
2015-10-23 15:52:27 +0300

    removed waste arrays

--- src/main/kotlin/hw05/main.kt
+++ src/main/kotlin/hw05/main.kt
@@ -4,18 +4,17 @@ import java.util.ArrayList
 
 public fun increment (numberOfThreads : Int, incr : Int) : Int{
    var result = 0
-   var threadArray : Array<Thread> = Array(numberOfThreads, {i -> Thread ()})
-   fun increase () { result++ }
    val number = incr / numberOfThreads
    val lastPart = incr - number * (numberOfThreads-1)
-   for (i in 0..numberOfThreads-1) {
-      threadArray[i] = Thread {
+   fun increase () { result++ }
+   var threadArray : Array<Thread> = Array(numberOfThreads, { i ->
+      Thread {
          for (k in 1..number) increase()
          if (i == numberOfThreads-1) {
             for (k in number+1..lastPart) increase()
          }
       }
-   }
+   })
    for (i in 0..numberOfThreads-1) {
       threadArray[i].start()
    }
@@ -25,64 +24,63 @@ public fun increment (numberOfThreads : Int, incr : Int) : Int{
    return result
 }
 
-private fun insert (array : ArrayList<Int>, value : Int) : ArrayList<Int> {
+private fun insert (array : ArrayList<Int>, value : Int, begin : Int, end : Int)
+        : ArrayList<Int> {
    var newArr = array
-   for (i in 0..array.size() - 1) {
+   for (i in begin..end) {
       if (value <= array[i]) {
          newArr.add(i,value)
          return newArr
       }
    }
-   newArr.add(array.size(), value)
+   newArr.add(end + 1, value)
    return newArr
 }
 
-private fun merge (array1 : ArrayList<Int>, array2 : ArrayList<Int>) : ArrayList<Int> {
-   var newArr = array1
-   for (i in 0..array2.size() - 1) {
-      newArr = insert(array1, array2[i])
+private fun merge (array : ArrayList<Int>, beg1 : Int, end1 : Int,
+                   beg2 : Int, end2 : Int) : ArrayList<Int> {
+   var newArr = array
+   for (i in beg2..end2) {
+      val v = newArr[i]
+      newArr.remove(i)
+      newArr = insert(array, v, beg1, end1 + i - beg2)
    }
    return newArr
 }
 
-private fun mergesort (array : ArrayList<Int>) : ArrayList<Int> {
-   var newArr = arrayListOf(arrayListOf (array[0]))
-   for (i in 1..array.size() - 1) { newArr.add(i, arrayListOf(array[i])) }
-   while (newArr.size() != 1) {
-      val size = newArr.size() / 2
-      for (i in 1..size) {
-         val k = (-i + size + 1) * 2 - 1
-         newArr[k - 1] = merge (newArr[k], newArr[k - 1])
-         newArr.remove(k)
+private fun mergesort (array : ArrayList<Int>,
+                       begin : Int, end : Int) : ArrayList<Int> {
+   var newArr = array
+   var size = 2
+   while (size <= end - begin + 1) {
+      for (i in 0..(end - begin + 1)/size - 1) {
+         newArr = merge (newArr, begin + i*size,  begin + i*size + size/2 - 1,
+                 begin + i*size + size/2,begin + (i+1)*size - 1)
       }
+      if (end - begin + 1 mod size != 0) {
+         val lastPart = (end - begin + 1) mod size
+         newArr = merge (newArr, begin, end - lastPart, end - lastPart + 1, end)
+      }
+      size = size * 2
    }
-   return newArr[0]
+   return newArr
 }
 
-public fun mergesortThreads (array : ArrayList<Int>, numberOfThreads : Int) : ArrayList<Int> {
-   var result : Array<ArrayList<Int>> = Array(numberOfThreads, {i -> arrayListOf<Int> ()})
-   var threadArray : Array<Thread> = Array(numberOfThreads, {i -> Thread ()})
+public fun mergesortThreads (array : ArrayList<Int>, numberOfThreads : Int)
+        : ArrayList<Int> {
+   var result = array
    val number = array.size() / numberOfThreads
-   val lastPart = array.size() - number * (numberOfThreads-1)
    for (i in 0..numberOfThreads-1) {
-      for (k in 0..number-1) {
-         result[i].add(array[i*number + k])
-      }
-      if (i == numberOfThreads - 1) {
-         for (k in number..lastPart - 1) {
-            result[i].add(array[i * number + k])
-         }
-      }
-      threadArray[i] = Thread {result[i] = mergesort(result[i])}
+      val thread = Thread {result = mergesort(result, i*number, (i+1)*number - 1)}
+      thread.start()
+      thread.join()
    }
-   for (i in 0..numberOfThreads-1) {
-      threadArray[i].start()
+   for (i in 1..numberOfThreads - 1){
+      result = merge (result, 0, number * i - 1, number * i, number * (i+1) - 1)
    }
-   for (i in 0..numberOfThreads-1) {
-      threadArray[i].join()
+   if (array.size() - number * numberOfThreads != 0) {
+      result = merge(result,0, number * numberOfThreads - 1,
+              number * numberOfThreads, result.size() - 1)
    }
-   for (i in 1..numberOfThreads-1) {
-      result[0] = merge (result[0], result[-i + numberOfThreads])
-   }
-   return result[0]
+   return result
 }
