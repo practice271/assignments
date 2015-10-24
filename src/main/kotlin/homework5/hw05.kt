2fcd153935ecaa98a031e384d0c5c78bf0e37bac
minonastya <minovskayanastya@gmail.com>
2015-10-24 18:05:46 +0300

    Changed Hw05

--- src/main/kotlin/homework5/hw05.kt
+++ src/main/kotlin/homework5/hw05.kt
@@ -13,91 +13,90 @@ public fun incWithoutSync(numberOfThreads : Int) : Int{
     return count
 }
 
-
-public fun mergeSort(arr:Array<Int>, numberOfThreads: Int): Array<Int> {
-        val size = arr.size()
-        when {
-            (size < 2) -> return arr
-            else -> {
-                val middle = size / 2
-                var left = Array(middle, { _ -> 0 })
-                var right = Array(size - middle, { _ -> 0 })
-                for (x in 0..middle - 1) {
-                    left[x] = arr[x]
+public fun sorting(arr:Array<Int>,  numberOfThreads: Int): Array<Int> {
+    var tmpArray = Array(arr.size(), {_ -> 0})
+    fun mergeSort(l: Int, r: Int, numberOfThreads: Int) {
+        val size = r - l + 1
+        val middle = l + size / 2
+        if (size >= 2) {
+            if (numberOfThreads > 1) {
+                val threadl = thread { mergeSort(l, middle - 1, numberOfThreads / 2) }
+                threadl.join()
+                mergeSort(middle, r, numberOfThreads / 2)
+            }
+            else {
+                mergeSort(l, middle - 1, numberOfThreads)
+                mergeSort(middle, r, numberOfThreads)
+            }
+            var i = l
+            var j = middle
+            while ((i < middle) && (j <= r)) {
+                when {
+                    arr[i] <= arr[j] -> {
+                        tmpArray[i + j - middle] = arr[i]; i++
+                    }
+                    arr[i] > arr[j] -> {
+                        tmpArray[i + j - middle] = arr[j]; j++
+                    }
                 }
-                for (x in middle..size - 1) {
-                    right[x - middle] = arr[x]
+            }
+            if (i < middle) {
+                for (x in i..middle - 1) {
+                    tmpArray[i + j - middle] = arr[x]
+                    i++
                 }
-                if (numberOfThreads > 1) {
-                    val threadl = thread { left = mergeSort(left, numberOfThreads - 1)}
-                    threadl.join()
-                    right = mergeSort(right, numberOfThreads - 1)
+            }
+            if (j <= r) {
+                for (x in j..r) {
+                    tmpArray[i + j - middle] = arr[x]
+                    j++
                 }
-                else {
-                    left = mergeSort(left, numberOfThreads)
-                    right = mergeSort(right, numberOfThreads)
-                }
-                var result = merge(left, right)
-                return result
+            }
+            for (x in l..r){
+                arr[x] = tmpArray[x]
             }
         }
-}
-internal  fun merge(left: Array<Int>, right: Array<Int>): Array<Int>{
-    var result = Array(left.size() + right.size(), {_ -> 0})
-    var l = 0
-    var r = 0
-    while ((l < left.size()) && (r < right.size())) {
-        when {
-            left[l] <= right[r] -> {result[l + r] = left[l]; l++}
-            left[l] > right[r] -> {result[l + r] = right[r]; r++}
-        }
     }
-    if (l < left.size()) {
-        for (x in l..left.size() - 1) {
-            result[l + r] = left[x]
-            l++
-        }
-    }
-    if (r < right.size()) {
-        for (x in r..right.size() - 1) {
-            result[l + r] = right[x]
-            r++
-        }
-    }
-    return result
+    mergeSort(0, arr.size() - 1, numberOfThreads )
+    return arr
 }
 
 private fun duration(size: Int, threadNum:Int):Long {
     val arr = Array(size, {_ -> Random().nextInt()})
-    return measureTimeMillis {mergeSort(arr, threadNum)  }
+    return measureTimeMillis {sorting(arr, threadNum)  }
 }
 
 /*
-Threads: 1	 Size: 10	     Time: 1
-Threads: 1	 Size: 100	     Time: 1
-Threads: 1	 Size: 1000	     Time: 6
-Threads: 1	 Size: 10000	 Time: 25
-Threads: 1	 Size: 100000	 Time: 163
-Threads: 1	 Size: 1000000	 Time: 1082
+Threads: 1	 Size: 10	     Time: 3
+Threads: 2	 Size: 10	     Time: 4
+Threads: 4	 Size: 10	     Time: 1
+Threads: 8	 Size: 10	     Time: 2
 
-Threads: 2	 Size: 10	     Time: 0
+Threads: 1	 Size: 100	     Time: 0
 Threads: 2	 Size: 100	     Time: 3
-Threads: 2	 Size: 1000	     Time: 12
-Threads: 2	 Size: 10000	 Time: 11
-Threads: 2	 Size: 100000	 Time: 150
-Threads: 2	 Size: 1000000	 Time: 1035
-
-Threads: 4	 Size: 10	     Time: 3
 Threads: 4	 Size: 100	     Time: 5
-Threads: 4	 Size: 1000	     Time: 35
-Threads: 4	 Size: 10000	 Time: 15
-Threads: 4	 Size: 100000	 Time: 138
-Threads: 4	 Size: 1000000	 Time: 754
+Threads: 8	 Size: 100	     Time: 5
 
-Threads: 6	 Size: 10	     Time: 2
-Threads: 6	 Size: 100	     Time: 8
-Threads: 6	 Size: 1000	     Time: 39
-Threads: 6	 Size: 10000	 Time: 71
-Threads: 6	 Size: 100000	 Time: 79
-Threads: 6	 Size: 1000000	 Time: 774
- */
+Threads: 1	 Size: 1000	     Time: 0
+Threads: 2	 Size: 1000	     Time: 7
+Threads: 4	 Size: 1000	     Time: 2
+Threads: 8	 Size: 1000	     Time: 7
+
+Threads: 1	 Size: 10000	 Time: 7
+Threads: 2	 Size: 10000	 Time: 4
+Threads: 4	 Size: 10000	 Time: 5
+Threads: 8	 Size: 10000	 Time: 6
+
+Threads: 1	 Size: 100000	 Time: 57
+Threads: 2	 Size: 100000	 Time: 66
+Threads: 4	 Size: 100000	 Time: 70
+Threads: 8	 Size: 100000	 Time: 88
+
+Threads: 1	 Size: 1000000	 Time: 752
+Threads: 2	 Size: 1000000	 Time: 589
+Threads: 4	 Size: 1000000	 Time: 480
+Threads: 8	 Size: 1000000	 Time: 500
+*/
+
+fun main(args: Array<String>) {
+}
--- src/test/kotlin/homework5/HW05.kt
+++ src/test/kotlin/homework5/HW05.kt
@@ -7,7 +7,7 @@ public class HW05 {
     //Test increment
     @Test
     fun testIncWithoutSync1(){
-       assert(10000 > incWithoutSync(10000))
+       assert(100000 > incWithoutSync(100000))
     }
     fun arrToStr(arr:Array<Int>):String{
         var s = ""
@@ -20,22 +20,22 @@ public class HW05 {
     @Test
     fun testMergeSort1(){
         val arr1 = arrayOf(1,3,4,6,5,7,8,9,2)
-        assertEquals(arrToStr(mergeSort(arr1, 4)), "1 2 3 4 5 6 7 8 9 ")
+        assertEquals(arrToStr(sorting(arr1, 4)), "1 2 3 4 5 6 7 8 9 ")
     }
     @Test
     fun testMergeSort2(){
         val arr2 = emptyArray<Int>()
-        assertEquals(arrToStr(mergeSort(arr2,2)), "")
+        assertEquals(arrToStr(sorting(arr2,2)), "")
     }
     @Test
     fun testMergeSort3(){
         val arr3 = arrayOf(1,2,1,2,1,2)
-        assertEquals(arrToStr(mergeSort(arr3, 1)), "1 1 1 2 2 2 ")
+        assertEquals(arrToStr(sorting(arr3, 1)), "1 1 1 2 2 2 ")
     }
     @Test
     fun testMergeSort4(){
         val arr4 = arrayOf(1,2,3, -1,-2,-3)
-        assertEquals(arrToStr(mergeSort(arr4, 3)), "-3 -2 -1 1 2 3 ")
+        assertEquals(arrToStr(sorting(arr4, 3)), "-3 -2 -1 1 2 3 ")
     }
 }
 
