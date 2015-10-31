8ac82abd56debeca267582b61c3cbcc32f7600cc
AnnaAK <anna.kudryashova.va@gmail.com>
2015-10-31 22:51:00 +0300

    delete Kotlin file

--- src/main/kotlin/hw07/Main.java
+++ src/main/kotlin/hw07/Main.java
@@ -2,7 +2,7 @@ package hw07;
 
 
 public class Main {
-    /**public static void main(String[] args) {
+    public static void main(String[] args) {
         ArrayOrdList<String> arr = new ArrayOrdList<>(1, "zebra");
         System.out.println("Size = " + (arr.size()));
         arr.add("monkey");
@@ -14,5 +14,5 @@ public class Main {
         arr.remove(2);
         arr.print();
         System.out.println("hashcode arr:  " + arr.hashcode());
-    }*/
+    }
 }
