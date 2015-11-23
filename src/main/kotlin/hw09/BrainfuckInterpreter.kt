61504995c6163d895cfa911b8328f8d5937fc4ab
onewhl <prystjam@gmail.com>
2015-11-23 18:45:36 +0300

    renamed some variables

--- src/main/kotlin/hw09/BrainfuckInterpreter.kt
+++ src/main/kotlin/hw09/BrainfuckInterpreter.kt
@@ -4,17 +4,18 @@ import java.util.*
 public class BrainfuckInterpreter {
     private val sc = Scanner(System.`in`)
     private val LENGTH = 10000
-    private fun newArray(size : Int) = Array(size, { 0 })
     private val memory = newArray(LENGTH)
     private var dataPointer: Int = 0
     public  var word = ""
 
+    private fun newArray(size: Int) = Array(size, { 0 })
+
     fun interpret(code: String): String {
-        var l = 0
-        var i = 0
-        var ind=0
-        while (i < code.length) {
-            when (code[i]) {
+        var levelLoop = 0
+        var index = 0
+
+        while (index < code.length) {
+            when (code[index]) {
                 '>' -> {
                     dataPointer = if ((dataPointer == LENGTH - 1)) 0 else dataPointer + 1
                 }
@@ -29,34 +30,33 @@ public class BrainfuckInterpreter {
                 }
                 '.' -> {
                     word+= memory[dataPointer].toChar()
-                    ind++
                 }
                 ',' -> {
                     memory[dataPointer] = sc.next().toInt()
                 }
                 '[' -> {
                     if (memory[dataPointer] == 0) {
-                        i++
-                        while (l > 0 || code[i] != ']') {
-                            if (code[i] == '[') l++
-                            if (code[i] == ']') l--
-                            i++
+                        index++
+                        while (levelLoop > 0 || code[index] != ']') {
+                            if (code[index] == '[') levelLoop++
+                            if (code[index] == ']') levelLoop--
+                            index++
                         }
                     }
                 }
                 ']' -> {
                     if (memory[dataPointer] != 0) {
-                        i--
-                        while (l > 0 || code[i] != '[') {
-                            if (code[i] == ']') l++
-                            if (code[i] == '[') l--
-                            i--
+                        index--
+                        while (levelLoop > 0 || code[index] != '[') {
+                            if (code[index] == ']') levelLoop++
+                            if (code[index] == '[') levelLoop--
+                            index--
                         }
-                        i--
+                        index--
                     }
                 }
             }
-            i++
+            index++
         }
         return word
     }
