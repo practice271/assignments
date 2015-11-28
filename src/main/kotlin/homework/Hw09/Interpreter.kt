1386bf8c199c7e162d5668b92d4bdb6cdd75ed64
Bulgakov Kirill <kbulgakov@yahoo.com>
2015-11-29 02:13:31 +0400

    Fixed

--- src/main/kotlin/homework/Hw09/Interpreter.kt
+++ src/main/kotlin/homework/Hw09/Interpreter.kt
@@ -1,36 +1,22 @@
 package homework.Hw09
+import java.io.Reader
 
-class Interpreter(var string:String){
+class Interpreter(){
     private var pointer = 0
-    private var charCounter = 0
     private var buffer = Array<Byte>(30000) {0}
-    private var result = Array<Char>(1000) {'0'}
 
-    //Support method to return value from @Interpret()
-    private fun getResult():String{
-        var temp = ""
-        for (i in 0..charCounter-1){
-            temp+=result[i].toChar()
-        }
-        return temp
-    }
-
-    public fun Interpret():String{
-//        if (string.length == 0) return ""
+    public fun Interpret(string:String, reader : Reader):String{
         var bracketBalance = 0
         var i = 0
+        var result = ""
         while(i < string.length){
             when(string[i]){
                 '>' -> pointer++
                 '<' -> pointer--
                 '+' -> buffer[pointer]++
                 '-' -> buffer[pointer]--
-                '.' -> {
-                    print(buffer[pointer].toChar())
-                    result[charCounter] = buffer[pointer].toChar()
-                    charCounter++
-                }
-                ',' -> buffer[pointer] = System.`in`.read().toByte()
+                '.' -> result += buffer[pointer].toChar()
+                ',' -> buffer[pointer] = reader.read().toByte()
                 '[' -> if (buffer[pointer] == 0.toByte()) {
                             i++
                             while (bracketBalance > 0 || string[i] != ']') {
@@ -51,6 +37,6 @@ class Interpreter(var string:String){
             }
             i++
         }
-        return getResult()
+        return result
     }
 }
--- src/test/kotlin/Homework/Hw09/test.kt
+++ src/test/kotlin/Homework/Hw09/test.kt
@@ -3,27 +3,42 @@ package Homework.Hw09
 import kotlin.test.assertEquals
 import org.junit.Test
 import homework.Hw09.Interpreter
-
+import java.io.Reader
 
 public class Hw09Test{
     @Test fun Test1(){
-        val obj = Interpreter("-[------->+<]>-.-[->+++++<]>++.+++++++" +
+        val res = Interpreter().Interpret("-[------->+<]>-.-[->+++++<]>++.+++++++" +
                 "..+++.[--->+<]>-----.---[->+++<]>.-" +
                 "[--->+<]>---.+++.------.---" +
-                "-----.-[--->+<]>.")
-        val res = obj.Interpret()
+                "-----.-[--->+<]>.",StringReader(""))
         val text = "Hello World!"
         assertEquals(res, text)
     }
 
     @Test fun Test2(){
-        val obj = Interpreter("-[------->+<]>++.-[-->+++<]>.+++++.--------" +
+        val res = Interpreter().Interpret("-[------->+<]>++.-[-->+++<]>.+++++.--------" +
                 ".---.+++++.-[->+++++<]>-.[-->+++++++<]>.++.---.--------.+++++" +
                 "++++++.+++[->+++<]>++.++++++++++++..----.+++++.-------.-[--->+<]>" +
                 "--.++[--->++<]>.-----------.+++++++++++++.--" +
-                "-----.--[--->+<]>--.[->+++<]>++.++++++.--.")
-        val res = obj.Interpret()
+                "-----.--[--->+<]>--.[->+++<]>++.++++++.--.", StringReader(""))
         val text = "Kotlin programming language"
         assertEquals(res, text)
+    }
+
+    @Test fun TestInput(){
+        val res = Interpreter().Interpret(",.>,.>,.>,.>,.>,.", StringReader("Kotlin"))
+        val text = "Kotlin"
+        assertEquals(text,res)
+    }
+
+    class StringReader(private val str : String) : Reader() {
+        var ptr = 0
+        override fun read() = if (ptr < str.length) str[ptr++].toInt() else -1
+        override fun read(cbuf : CharArray?, off : Int, len : Int) : Int {
+            throw UnsupportedOperationException()
+        }
+
+        override fun close() {
+        }
     }
 }
