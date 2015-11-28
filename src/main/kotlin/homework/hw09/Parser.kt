be7cab2dcfbb321069e216e931e2fb14d1da5fa0
Sharganov Artem <sharganov.artem@yandex.ru>
2015-11-29 02:35:21 +0300

    improve according to recommendation

--- src/main/kotlin/homework/hw09/Parser.kt
+++ src/main/kotlin/homework/hw09/Parser.kt
@@ -7,21 +7,22 @@ public class Parser() {
     public class IncorrectInputException() : Throwable()
     public class IncorrectBracketsSequence() : Throwable()
 
-    enum class Tokens { lshift, rshift, INC, DEC, print, write, lb, rb }
+    enum class Tokens {LS, RS, INC, DEC, PRINT, WRITE, LB, RB }
 
     private val tokenList = ArrayList<Tokens>()
 
     public fun parse(input: String) : ArrayList<Tokens> {
         var balance = 0
         for (char in input) when (char) {
-            '>' -> tokenList.add(Tokens.rshift)
-            '<' -> tokenList.add(Tokens.lshift)
+            '>' -> tokenList.add(Tokens.RS)
+            '<' -> tokenList.add(Tokens.LS)
             '+' -> tokenList.add(Tokens.INC)
             '-' -> tokenList.add(Tokens.DEC)
-            ',' -> tokenList.add(Tokens.write)
-            '.' -> tokenList.add(Tokens.print)
-            '[' -> {tokenList.add(Tokens.lb); balance -= 1}
-            ']' -> {tokenList.add(Tokens.rb); if(balance>=0) throw IncorrectBracketsSequence() else balance += 1 }
+            ',' -> tokenList.add(Tokens.WRITE)
+            '.' -> tokenList.add(Tokens.PRINT)
+            '[' -> {tokenList.add(Tokens.LB); balance -= 1}
+            ']' -> {tokenList.add(Tokens.RB); if(balance >= 0)
+                throw IncorrectBracketsSequence() else balance += 1 }
             ' ' -> true
             else -> throw IncorrectInputException()
         }
--- src/test/kotlin/homework/hw09/InterpreterTest.kt
+++ src/test/kotlin/homework/hw09/InterpreterTest.kt
@@ -7,53 +7,16 @@ import kotlin.test.assertEquals
 class InterpreterTest {
 
     //the differencies are in way of output
-    public class TestInterpreter(private val str : String) {
 
-        private var arr = ByteArray(100)
-        private var pointer = 0
-        private val tokens = Parser().parse(str)
-        private var level = 0
-        private var i = 0
 
-        public fun interpret() : String {
-
-            var answer = ""
-            while (i < tokens.size) {
-                when (tokens[i]) {
-                    Parser.Tokens.rshift -> pointer++
-                    Parser.Tokens.lshift -> pointer--
-                    Parser.Tokens.INC -> arr[pointer]++
-                    Parser.Tokens.DEC -> arr[pointer]--
-                    Parser.Tokens.write  -> arr[pointer] = System.`in`.read().toByte()
-                    Parser.Tokens.print  -> answer += arr[pointer].toChar()
-                    Parser.Tokens.lb ->
-                        if (arr[pointer].toInt() == 0) {
-                            i++
-                            while (level > 0 || tokens[i] != Parser.Tokens.rb) {
-                                if (tokens[i] == Parser.Tokens.lb) level++
-                                if (tokens[i] == Parser.Tokens.rb) level--
-                                i++
-                            }
-                        }
-                    Parser.Tokens.rb ->
-                        if (arr[pointer].toInt() != 0) {
-                            i--
-                            while (level > 0 || tokens[i] != Parser.Tokens.lb) {
-                                if (tokens[i] == Parser.Tokens.rb) level++
-                                if (tokens[i] == Parser.Tokens.lb) level--
-                                i--
-                            }
-                            i--
-                        }
-                }
-                i++
-            }
-            return answer
-        }
+    @Test fun helloWorldTest(){
+        val interpreter = Brainfuck("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
+        assertEquals("Hello World!\n",interpreter.interpret(""))
     }
+    //simple input test
+    @Test fun InputTest(){
+        val interpreter = Brainfuck(",.")
+        assertEquals("0", interpreter.interpret("0"))
 
-    @Test fun helloworldTest1(){
-        val interpreter = TestInterpreter("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
-        assertEquals("Hello World!\n",interpreter.interpret())
     }
 }
