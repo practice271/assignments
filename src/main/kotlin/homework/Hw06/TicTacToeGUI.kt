7a443e71e84922c19ec4f8f81d805bf8f5f62402
Kirill <kbulgakov@yahoo.com>
2015-12-29 00:34:07 +0300

    Update TicTacToeGUI.kt

--- src/main/kotlin/homework/Hw06/TicTacToeGUI.kt
+++ src/main/kotlin/homework/Hw06/TicTacToeGUI.kt
@@ -9,7 +9,13 @@ import java.util.*
 public class TicTacToeGame : JFrame() {
     private var board = HashMap<Pair<Int, Int>, String> ()
     val step = Step("0")
-
+    val game = Game()
+    private val gridxSmall = 0
+    private val gridxMedium = 4
+    private val gridxBig = 8
+    private val gridxLarge = 12
+    private val gridWidthSmall = 3
+    private val gridWidthLarge = 7
     private val WIDTH = 800
     private val HEIGHT = 650
     private val newGameBtn = JButton("New game")
@@ -48,27 +54,27 @@ public class TicTacToeGame : JFrame() {
 
         temp.insets = Insets(10,0,0,0)
         // up down left right
-        temp.gridwidth = 3; temp.gridx = 0; temp.gridy = fieldSize + 1
+        temp.gridwidth = gridWidthSmall; temp.gridx = gridxSmall; temp.gridy = fieldSize + 1
         upBtn.preferredSize = Dimension(75,25); upBtn.isFocusable = false
         pane.add(upBtn, temp)
-        temp.gridwidth = 3; temp.gridx = 4; temp.gridy = fieldSize + 1
+        temp.gridwidth = gridWidthSmall; temp.gridx = gridxMedium; temp.gridy = fieldSize + 1
         downBtn.preferredSize = Dimension(75,25); downBtn.isFocusable = false
         pane.add(downBtn, temp)
-        temp.gridwidth = 3; temp.gridx = 8; temp.gridy = fieldSize + 1
+        temp.gridwidth = gridWidthSmall; temp.gridx = gridxLarge; temp.gridy = fieldSize + 1
         leftBtn.preferredSize = Dimension(75,25); leftBtn.isFocusable = false
         pane.add(leftBtn, temp)
-        temp.gridwidth = 3; temp.gridx = 12; temp.gridy = fieldSize + 1
+        temp.gridwidth = gridWidthSmall; temp.gridx = gridxLarge; temp.gridy = fieldSize + 1
         rightBtn.preferredSize = Dimension(75,25); rightBtn.isFocusable = false
         pane.add(rightBtn, temp)
         // status
-        temp.gridwidth = 7; temp.gridx = 4; temp.gridy = fieldSize + 2
+        temp.gridwidth = gridWidthLarge; temp.gridx = gridxMedium; temp.gridy = fieldSize + 2
         pane.add(status, temp)
         // newGame
-        temp.gridwidth = 4; temp.gridx = 0; temp.gridy = fieldSize + 2
+        temp.gridwidth = gridxMedium; temp.gridx = gridxSmall; temp.gridy = fieldSize + 2
         newGameBtn.preferredSize = Dimension(100,25); newGameBtn.isFocusable = false
         pane.add(newGameBtn, temp)
         // exit
-        temp.gridwidth = 4; temp.gridx = fieldSize - 4; temp.gridy = fieldSize + 2
+        temp.gridwidth = gridxMedium; temp.gridx = fieldSize - 4; temp.gridy = fieldSize + 2
         exitBtn.preferredSize = Dimension(100,25); exitBtn.isFocusable = false
         pane.add(exitBtn, temp)
         setSize(WIDTH, HEIGHT)
@@ -79,7 +85,7 @@ public class TicTacToeGame : JFrame() {
         downBtn.addActionListener({centerX++;writeBoard()})
         leftBtn.addActionListener({centerY--;writeBoard()})
         rightBtn.addActionListener({centerY++;writeBoard()})
-        newGameBtn.addActionListener({board = HashMap<Pair<Int, Int>, String> ();
+        newGameBtn.addActionListener({game.board = HashMap<Pair<Int, Int>, String> ();
             writeBoard();
             status.text = "";})
     }
@@ -97,30 +103,11 @@ public class TicTacToeGame : JFrame() {
         }
     }
 
-    private fun winCheck(c:Pair<Int,Int>):String{
-        val x = c.first
-        val y = c.second
-        var counters = arrayOf(1,1,1,1,1,1,1,1)
-        var mark = board?.get(c) ?: ""
-        for (i in 1..4){
-            if (board.get(Pair(x,y-i)) == mark) counters[0]++
-            if (board.get(Pair(x,y+i)) == mark) counters[1]++
-            if (board.get(Pair(x-i,y)) == mark) counters[2]++
-            if (board.get(Pair(x+i,y)) == mark) counters[3]++
-            if (board.get(Pair(x+i,y+i)) == mark) counters[4]++
-            if (board.get(Pair(x+i,y-i)) == mark) counters[5]++
-            if (board.get(Pair(x-i,y-i)) == mark) counters[6]++
-            if (board.get(Pair(x-i,y+i)) == mark) counters[7]++
-        }
-        if (counters.contains(5)) return mark
-        return "-"
-    }
-
     private fun writeBoard(){
         for (i in 0..fieldSize-1){
             for (j in 0..fieldSize-1){
                 val key = Pair(centerX + i,centerY + j)
-                fieldBtn[i][j].text = board?.get(key) ?: ""
+                fieldBtn[i][j].text = game.board?.get(key) ?: ""
                 if (fieldBtn[i][j].text != "" && status.text == "") fieldBtn[i][j].isEnabled = false
                 else fieldBtn[i][j].isEnabled = true
             }
